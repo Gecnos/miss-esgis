330260713ecf8e7b337f4cc634324eedd77d534a
Florent BOUDZOUMOU <109081275+Florent242@users.noreply.github.com>
2025-08-05 20:33:46 +0200

    mise en place des migrations en fonctions du sql et des dependances necessaires a la bdd

---
+++ database/migrations/2025_08_05_174806_create_votes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('miss_id')->constrained('misses')->onDelete('cascade');
+            $table->string('moyen_paiement', 50)->nullable();
+            $table->decimal('montant', 10, 2)->nullable();
+            $table->timestamp('timestamp')->useCurrent();
+            $table->string('numero_telephone', 20)->nullable();
+            $table->string('email', 255)->nullable();
+            $table->string('ip_adresse', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+};
